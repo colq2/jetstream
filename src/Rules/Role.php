2d1c3e80ab9bd5664fa59a47d9832a8d52b2df82
Oliver Wycisk <oliver.wycisk@gmail.com>
2024-06-05 15:45:56 +0200

    Update role rule from deprecated Rule to ValidationRule

--- src/Rules/Role.php
+++ src/Rules/Role.php
@@ -2,30 +2,24 @@
 
 namespace Laravel\Jetstream\Rules;
 
-use Illuminate\Contracts\Validation\Rule;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
 use Laravel\Jetstream\Jetstream;
 
-class Role implements Rule
+class Role implements ValidationRule
 {
     /**
-     * Determine if the validation rule passes.
+     * Run the validation rule.
      *
      * @param  string  $attribute
      * @param  mixed  $value
-     * @return bool
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @return void
      */
-    public function passes($attribute, $value)
+    public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        return in_array($value, array_keys(Jetstream::$roles));
-    }
-
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function message()
-    {
-        return __('The :attribute must be a valid role.');
+        if(!in_array($value, array_keys(Jetstream::$roles))) {
+            $fail(__('The :attribute must be a valid role.'));
+        }
     }
 }
